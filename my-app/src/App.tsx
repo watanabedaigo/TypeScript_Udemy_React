304f0d1641128b6608231df3ab4c5997eec61caf
渡邊代悟 <wdaigm0801@gmail.com>
2022-12-12 12:45:50 +0900

    関数コンポーネント型指定

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,25 +2,35 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+interface AppProps {
+  message: string;
+  name: string;
+}
+
+// 引数にて分割代入を用いて変数に特定のプロパティの値を格納
+// 関数コンポーネントは :React.FC<> で型指定。ジェネリクスで渡した型引数がpropsの型になる。
+const App: React.FC<AppProps> = ({ message, name }) => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>{name}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {message}
         </a>
       </header>
     </div>
   );
-}
+};
+
+// 値が渡されなかった際のデフォルトのpropsを設定することができる
+App.defaultProps = {
+  message: 'defaultProps',
+};
 
 export default App;
